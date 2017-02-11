a6f80b1748c45c835bf7522e3ebed7c287c7a034
yaniv-l <yanivl@srl.co.il>
2017-02-11 10:48:59 +0200

    Added comments and debug for componentWillUpdate

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -24,7 +24,12 @@ export default function (ComposedComponent) {
     // update in which we can revalidate the
     componentWillUpdate(nextProps) {
       // nextProps is the updated state under which the componente has been
-      // updtaed
+      // updtaed and represents the related props that are going to be updated
+      // but are yet update - i.e a king of beforePropsCommit - thus, if we'll
+      // check this.props.authenticated now it will still be in its current
+      // (soon to be previous) value - uncomment the below console.log to see
+      console.log('Current authenticated: ', this.props.authenticated,
+        ' Next: ', nextProps.authenticated);  // For debug
       if (!nextProps.authenticated) {
         this.context.router.push('/');
       }
